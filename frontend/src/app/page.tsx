c7230d3120ada0b202dae442fec38492508a0347
Maureen Njihia <maureennjihia468@gmail.com>
2025-07-16 19:40:19 +0300

    Simplify Home component by removing unnecessary wrapper div around YubiKeyManager

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,9 +2,5 @@ import YubiKeyManager from "@/components/YubiKeyManager";
 
 
 export default function Home() {
-  return (
-      <div className="min-h-screen bg-gray-50">
-        <YubiKeyManager />
-      </div>
-  );
+  return <YubiKeyManager />;
 }
